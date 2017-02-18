9d7e3dfd4fa9f8f4a677243547d8155131e967cf
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-02-18 09:43:11 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+import re
+main_py = open('textpipes/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", main_py))
+
+#requires = [ ]
+
+setup(name='textpipes',
+      version=metadata['version'],
+      author=metadata['author'],
+      author_email='stig-arne.gronroos@aalto.fi',
+      #url='',
+      description='textpipes',
+      packages=['textpipes'], #'textpipes.tests'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          #'Topic :: Scientific/Engineering',
+      ],
+      license="BSD",
+      #scripts=['scripts/textpipes',
+      #        ],
+      #install_requires=requires,
+      #extras_require={
+      #    'docs': [l.strip() for l in open('docs/build_requirements.txt')]
+      #}
+      )
