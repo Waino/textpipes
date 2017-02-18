c8a606e54a3622cc5da90c628b1cb258051146b3
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-02-18 10:04:13 +0200

    initial Recipe

---
+++ textpipes/recipe.py
@@ -0,0 +1,11 @@
+class Recipe(object):
+    """Main class for building experiment recipes"""
+    def __init__(self):
+        pass
+
+    def status(self, conf, outfile):
+        # -> Done
+        # or Available(step, infiles)
+        # or Needs(infiles)
+        # or NoRecipe
+        pass
