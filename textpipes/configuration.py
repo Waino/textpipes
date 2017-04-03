1ecb733fca6a95134c305885d7f1e47e762b506d
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-04-03 18:38:23 +0300

    minor

--- textpipes/configuration.py
+++ textpipes/configuration.py
@@ -28,9 +28,7 @@ class Config(object):
         return conf
         
 
-# clunky: can use dict notation
-# '{FILE[corpus]}.{resection[some]}.{resection[wtf]}.{resection[lulz]}.gz'.format(**conf)
-# alternative: use interpolation in configparser
+# use interpolation in configparser
 # '${FILE:corpus}.${resection:some}'
 # can also include dollar-free, which can be formatted from command line args
 # '${resection:some}.mb{minibatch}'
