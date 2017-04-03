fd5506b4cd26cfbbbb8e1213886f58fd778fffe6
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-04-03 18:36:40 +0300

    subconfigs and platform configs

--- textpipes/configuration.py
+++ textpipes/configuration.py
@@ -2,13 +2,31 @@ import configparser
 
 class Config(object):
     def __init__(self, main_conf_file):
-        self.conf = configparser.ConfigParser(
-            interpolation=configparser.ExtendedInterpolation())
+        self.platform = None
+        self.conf = self.platform_config()
         self.conf.read_file(open(main_conf_file, 'r'))
-        # FIXME: for all keys in subconf section, read and union
+        if 'subconf' in self.conf:
+            for (key, subconf) in self.conf['subconf'].items():
+                self.conf.read_file(open(subconf, 'r'))
 
     def get_path(self, section, key):
         return self.conf['paths.{}'.format(section)][key]
+
+    def platform_config(self):
+        try:
+            with open('current_platform', 'r') as fobj:
+                self.platform = fobj.readline().strip()
+        except FileNotFoundError:
+            raise Exception(
+                'Expecting to find a file named "current_platform" '
+                'in the working directory. It should contain a string, '
+                'which when plugged into platform_{}.ini points to a '
+                'valid platform config.')
+        conf = configparser.ConfigParser(
+            interpolation=configparser.ExtendedInterpolation())
+        conf.read_file(open('platform_{}.ini'.format(self.platform), 'r'))
+        return conf
+        
 
 # clunky: can use dict notation
 # '{FILE[corpus]}.{resection[some]}.{resection[wtf]}.{resection[lulz]}.gz'.format(**conf)
