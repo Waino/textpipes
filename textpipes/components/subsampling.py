ab4acd3592c16921babd5776b0294f4f5c65538b
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-04-06 17:47:26 +0300

    head component

---
+++ textpipes/components/subsampling.py
@@ -0,0 +1,11 @@
+from .core import MonoPipeComponent
+
+class Head(MonoPipeComponent):
+    def __init__(self, limit):
+        self.limit = limit
+
+    def __call__(self, stream):
+        for (i, line) in enumerate(lines):
+            if i >= self.limit:
+                break
+            yield line
