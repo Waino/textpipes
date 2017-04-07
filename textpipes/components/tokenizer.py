8588371ccaea00cdeb420011333e368f8f6aa42f
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-04-07 11:11:13 +0300

    old truecaser and tokenizer

---
+++ textpipes/components/tokenizer.py
@@ -0,0 +1,188 @@
+import codecs
+import re
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+from .core import SingleCellComponent
+
+MULTISPACE_RE = re.compile(r' +')
+END_PERIOD_RE = re.compile(r'\.\s*$')
+# punctuation that should be tokenized separately
+TOK_PUNC_RE = re.compile(r'([\.,!?:;/@%\(\)\'"+£\$€])')
+
+# FIXME: use package resources instead
+LANG_DIR = os.path.join(
+    os.path.dirname(__file__), 'langs')
+
+def read_list_file(fname, lang):
+    result = []
+    path = os.path.join(LANG_DIR, '{}.{}'.format(fname, lang))
+    for line in codecs.open(path, encoding='utf-8'):
+        line = line.strip()
+        if len(line) == 0 or line[0] == '#':
+            continue
+        result.append(line)
+    return result
+
+# Tokenization must be mostly reversible for use on target lang:
+# not good to split hyphens here
+# r'- \d',           # negative numbers (protected)
+class Tokenize(SingleCellComponent):
+    def __init__(self, lang):
+        super().__init__()
+        self.lang = lang
+        # FIXME: customizable punctuation?
+        self.punctuation_re = TOK_PUNC_RE
+        self.protected_str = read_list_file('nonbreaking_prefix', self.lang)
+        # these are specified in the over-tokenized form
+        # all contained spaces are removed
+        self.protected_re = [
+            # decimal and thousand separators, IPs (note: non-grouping)
+            r'\d (?:[\.,] \d)+',
+            #r'\d %',           # percentages
+            r'\.(?: \.)+',      # multiple dots (note: non-grouping)
+            # urls. stops at first original space (note: not rawstring)
+            'https? : / / [^\u001F]*',
+            # certain domains even if not urls
+            r'(?:www \. )?[A-Za-z]* \. (?:com|cz|de|fi|org|ru|tr)',
+            # emails. stops at first original space
+            r'[a-z\. ]* @ [a-z\. ]*',
+            ]
+        self.map_re = [
+            # Abbreviations protected if followed by a number.
+            (r'(No|Art|pp) \.(\s+\d)', r"\1.\2"),
+            # name clitics: O' D' (don't split at all)
+            (r' ([OD])\s+\' (?=\w)', r" \1'"),
+            # decimals without leading zero: .38 caliber
+            # must originally have a leading space
+            ('(\u001F) ?\\. (\\d)', r'\1 .\2'),
+            ]
+        if self.lang == 'en':
+            self.map_re.extend(
+                ((r'(?<=\w) \'\s+([a-z]) ', r" '\1 "),    # english suffix clitics. FIXME: fr and others
+                 (r'(?<=\w) \'\s+(ll|re|ve) ', r" '\1 "), # longer clitics: 'll 're 've
+                 (r'(?<=\d) \'\s+s ', r" 's "),           # special case: 1990's
+                ))
+        elif self.lang == 'fi':
+            self.protected_re.extend(
+                (r'\d \. ',  # ordinals
+                ))
+            # not trying to correct if split in input
+            self.map_re.extend(
+                ((r'(?<=\w) : ([a-zåäö]{1,3}) ', r" :\1 "),    # finnish abbrevation suffixes
+                 (r'(?<=\w) : (nneksi|ista) ', r" :\1 "),      # longer suffixes
+                ))
+
+        # process and compile expressions
+        self.protected_str = self._compile_str(self.protected_str)
+        self.protected_re = self._compile_re(self.protected_re)
+        self.map_re = self._compile_map(self.map_re)
+
+    def single_cell(self, sentence):
+        out = sentence
+        # mark original spaces
+        out = out.replace(' ', ' \u001F ')
+        out = ' ' + out + ' '
+        out = self._split(out)
+        # collapse multiple spaces
+        out = MULTISPACE_RE.sub(' ', out)
+        # recombine protected
+        for (src, tgt) in self.protected_str:
+            out = out.replace(src, tgt)
+        for pattern in self.protected_re:
+            for src in self._unique(pattern.findall(out)):
+                tgt = src.replace(' ', '')
+                out = out.replace(src, tgt)
+        # do mappings
+        for (src, tgt) in self.map_re:
+            out = src.sub(tgt, out)
+        # always split final period, even if abbrev.
+        #out = END_PERIOD_RE.sub(' .', out)
+        # remove original space markers
+        out = out.replace('\u001F', '')
+        # collapse multiple spaces
+        out = MULTISPACE_RE.sub(' ', out)
+        out = out.strip()
+        return out
+
+    def _split(self, string):
+        return self.punctuation_re.sub(r' \1 ', string)
+
+    def _compile_str(self, patterns):
+        result = []
+        for pattern in patterns:
+            # final period is not included in file
+            pattern += '.'
+            # rstrip leaves leading space: not enough that end of token matches pattern
+            result.append((' ' + self._split(pattern).rstrip(), ' ' + pattern))
+        return result
+
+    def _compile_re(self, patterns):
+        return [re.compile(pattern, flags=re.UNICODE) for pattern in patterns]
+
+    def _compile_map(self, patterns):
+        return [(re.compile(pattern, flags=re.UNICODE), tgt)
+                for (pattern, tgt) in patterns]
+
+    def _unique(self, matches):
+        """ Removes duplicate matches, and sorts from longer to shorter,
+        to avoid partially replacing longer matches.
+        """
+        matches = set(matches)
+        return sorted(matches, key=len, reverse=True)
+
+
+class DeTokenize(SingleCellComponent):
+    def __init__(self, lang):
+        super().__init__()
+        self.lang = lang
+        self.expressions = [
+            # collapse multispace
+            (r' +', ' '),
+        ]
+        if self.lang == 'en':
+            self.expressions.extend([
+                # english suffix clitics
+                (r' (\'[a-z]) ', r'\1 '),
+                (r' (\'(ll|re|ve)) ', r'\1 '),
+                # plural apostrophe for s-ending
+                (r'(s) (\') ', r'\1\2 '),
+                ])
+        elif self.lang == 'fi':
+            # finnish abbrevation suffixes
+            self.expressions.extend([
+                (r' (:[a-zåäö]{1,3}) ', r"\1 "),
+                (r' (:(nneksi|ista)) ', r"\1 "),
+                ])
+            # in finnish, would joining apos from both sides make sense?
+        # must come after clitics
+        self.expressions.extend([
+            # colon joined from both sides if between numbers
+            (r'(\d) : (\d)', r'\1:\2'),
+            # join left
+            (r' ([\.,!?:;%\]\)])(?!\w)', r'\1'),
+            # join right
+            (r'([@\[\(]) ', r'\1'),
+            # join both sides
+            #(r' ([/]) ', r'\1'),
+            # join currency symbol towards number
+            (r'([\$£€]) ([-\.,]*\d)', r'\1\2'),
+            (r'(\d[\.,]?) ([\$£€])', r'\1\2'),
+            # join paired quotes inward
+            (r'(") ([^"]+) (")', r'\1\2\3'),
+            # unpaired starting/ending quote
+            (r'^ (") ', r' \1'),
+            (r' (") $', r'\1 '),
+            # unpaired quote followed by punc joined right
+            (r' ("[\.,]) ', r'\1 '),
+            ])
+        self.expressions = [(re.compile(exp, flags=re.UNICODE), repl)
+                            for (exp, repl) in self.expressions]
+
+    def single_cell(self, val):
+        val = ' ' + val + ' '
+        for (exp, repl) in self.expressions:
+            val = exp.sub(repl, val)
+        return val.strip()
---
+++ textpipes/components/truecaser.py
@@ -0,0 +1,148 @@
+import codecs
+import re
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+from .core import SingleCellComponent
+
+ALNUM_RE = re.compile('\w', flags=re.UNICODE)
+# punctuation that is followed by uppercase
+# note that this is a smaller set than tokenizer punctuation
+# FIXME: excl and quest not if followed by closing quote or paren
+# (hard to implement, as matching is done by token
+CASE_PUNC_RE = re.compile(r'^([\.!?:])$')   # FIXME: semicolon?
+
+# FIXME: use package resources instead
+LANG_DIR = os.path.join(
+    os.path.dirname(__file__), 'langs')
+
+class TrainTrueCaser(SingleCellComponent):
+    def __init__(self, model_file, sure_thresh=.6)
+        # sure_thresh: truecase also within sentence if common enough
+        super().__init__(side_outputs=[model_file])
+        self.model_file = model_file
+        self.sure_thresh = sure_thresh
+        self.counts = collections.defaultdict(collections.Counter)
+
+    def single_cell(self, sentence):
+            seen_first = False
+            for token in sentence.split():
+                if not seen_first:
+                    # skip fully nonalnum tokens in beginning
+                    if ALNUM_RE.search(token):
+                        seen_first = True
+                    continue
+                elif self.punctuation_re.match(token):
+                    seen_first = False
+                lower = token.lower()
+                self.counts[lower][token] += 1
+                # FIXME: also count prefixes with reduced weight?
+
+    def save(self):
+        self.words = dict()
+        for (word, counts) in self.counts.items():
+            # FIXME: use prefix counts
+            total = sum(counts.values())
+            best, bestcount = counts.most_common(1)[0]
+            sure = (float(bestcount) / total) > self.sure_thresh
+            self.words[word] = (best, sure)
+        # yield model serialized into rows
+        # FIXME: write into file?
+        for (word, (best, sure)) in self.words.items():
+            yield (word, best, str(sure))
+
+
+class TrueCase(SingleCellComponent):
+    def __init__(self, model_file,
+                 titlecase_thresh=.5, lc_unseen_first=False):
+        # sure_thresh: truecase also within sentence if common enough
+        super().__init__(side_inputs=[model_file])
+        self.model_file = model_file
+        self.titlecase_thresh = titlecase_thresh
+        self.lc_unseen_first = lc_unseen_first
+        self.words = None
+        # punctuation that resets seen_first
+        # note that this is a smaller set than tokenizer punctuation
+        self.punctuation_re = CASE_PUNC_RE
+
+    def load(self):
+        self.counts = None
+        self.words = dict()
+        for (i, tpl) in enumerate(self.persist_file.read()):
+            try:
+                (word, best, sure) = tpl
+                sure = (sure == 'True')
+            except ValueError:
+                raise Exception(
+                    'Unable to load truecaser line {} "{}"'.format(i, tpl))
+            self.words[word] = (best, sure)
+        return None
+
+    def single_cell(self, sentence):
+        result = []
+        seen_first = False
+        tokens = sentence.split()
+        lowered = [token.lower() for token in tokens]
+        n_cased = sum(token != lower
+                      for (token, lower)
+                      in zip(tokens, lowered))
+        # FIXME: titlecase: don't count lang-specific always-lower words
+        # FIXME: ignore very short sentences
+        n_maybetitle = sum(1 for token in tokens
+                           if ALNUM_RE.search(token))
+        aggressive = (
+            n_cased == 0    # irc-case
+            or float(n_cased) / n_maybetitle > self.titlecase_thresh)
+        for (token, lower) in zip(tokens, lowered):
+            if lower in self.words:
+                best, sure = self.words[lower]
+                if sure and (aggressive or not seen_first):
+                    # truecase the first token
+                    # truecase also within sentence if abnormal case
+                    truecased = best
+                else:
+                    # if not sure, don't change the case
+                    truecased = token
+            else:
+                if (not seen_first) and self.lc_unseen_first:
+                    # lowercase unseen first words
+                    truecased = lower
+                else:
+                    # if not sure, don't change the case
+                    truecased = token
+            result.append(truecased)
+            if not seen_first and ALNUM_RE.search(token):
+                seen_first = True
+            elif self.punctuation_re.match(token):
+                seen_first = False
+        return ' '.join(result)
+
+
+# FIXME: detruecase: MWE:s
+# FIXME: using LM, alignment to source, ...
+class DeTrueCase(SingleCellOperation):
+    def __init__(self):
+        super().__init__()
+        #self.lang = node_in.column_tags.get_joined('lang')
+        # FIXME: customizable punctuation?
+        self.punctuation_re = CASE_PUNC_RE
+
+    def single_cell(self, sentence, tags):
+        result = []
+        seen_first = False
+        tokens = sentence.split()
+        for token in tokens:
+            if not seen_first:
+                if ALNUM_RE.search(token):
+                    token = self._uc_first(token)
+                    seen_first = True
+            elif self.punctuation_re.match(token):
+                seen_first = False
+            result.append(token)
+        return ' '.join(result)
+
+    def _uc_first(self, token):
+        # can't use capitalize(), don't want to lower rest
+        return token[0].upper() + token[1:]
