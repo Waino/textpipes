13aeed59929ab5c64c8fa3799ee91d47be6e6be8
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-10-17 15:22:57 +0300

    hnmt vocabulary creation

--- textpipes/__init__.py
+++ textpipes/__init__.py
@@ -23,7 +23,7 @@ def get_version():
 
 from .core import *
 from . import components, check
-from . import finnpos
+from . import finnpos, hnmt
 
 # Most common rules for easy access
 from .dedup import Deduplicate
---
+++ textpipes/hnmt.py
@@ -0,0 +1,29 @@
+from .core.recipe import Rule
+from .core.platform import run
+
+class MakeVocabularies(Rule):
+    def __init__(self, *args, argstr='', **kwargs):
+        super()__init__(*args, **kwargs)
+        self.argstr = argstr
+
+    def make(self, conf, cli_args):
+        src_infile = self.inputs[0](conf, cli_args)
+        trg_infile = self.inputs[1](conf, cli_args)
+        src_outfile = self.outputs[0](conf, cli_args)
+        trg_outfile = self.outputs[1](conf, cli_args)
+        run('make_vocabularies.py {src_infile} {trg_infile}'
+            ' {src_outfile} {trg_outfile} {argstr}'.format(
+                src_infile=src_infile,
+                trg_infile=trg_infile,
+                src_outfile=src_outfile,
+                trg_outfile=trg_outfile,
+                argstr=self.argstr))
+
+class PrepareData(Rule):
+    pass
+
+class Train(Rule):
+    pass
+
+class Translate(Rule):
+    pass
