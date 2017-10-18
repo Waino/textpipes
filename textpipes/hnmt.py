e5bd4d962350216ca4f5bf1cf9c5c7ec1ec36a86
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-10-18 17:30:31 +0300

    hnmt train

--- textpipes/hnmt.py
+++ textpipes/hnmt.py
@@ -47,7 +47,53 @@ class PrepareData(Rule):
                 argstr=self.argstr))
 
 class Train(Rule):
-    pass
+    def __init__(self,
+                 shard_file, heldout_src, heldout_trg,
+                 log_file, model_seckey, loop_indices,
+                 aux_type='none', argstr=''):
+        models = LoopRecipeFile.loop_output(
+            model_seckey[0], model_seckey[1], loop_indices)
+        self.shard_file = shard_file
+        self.heldout_src = heldout_src
+        self.heldout_trg = heldout_trg
+        self.log_file = log_file
+        self.aux_type = aux_type
+        self.argstr = argstr
+
+        inputs = [shard_file, heldout_src, heldout_trg]
+        outputs = models + [log_file]
+        super().__init__(inputs, outputs)
+
+    def make(self, conf, cli_args):
+        # loop index is appended by hnmt to given path
+        model_base, _ = self.models[0](conf, cli_args).rsplit('.', 1)
+        run('hnmt.py --save-model {model_base}'
+            ' --train {shard_file}'
+            ' --heldout-source {heldout_src}'
+            ' --heldout-target {heldout_trg}'
+            ' --log-file {log_file}'
+            ' --aux-type {aux_type}'
+            ' {argstr}'.format(
+                model_base=model_base
+                shard_file=self.shard_file(conf, cli_args)
+                heldout_src=self.heldout_src(conf, cli_args)
+                heldout_trg=self.heldout_trg(conf, cli_args)
+                log_file=self.log_file(conf, cli_args)
+                aux_type=aux_type,
+                argstr=argstr))
+        #'--validate-every 5 --translate-every 5'
+
+    def is_atomic(self, output):
+        # all loop outputs are atomic
+        return isinstance(output, LoopRecipeFile)
+
+    def monitor(self, platform, conf, cli_args=None):
+        highest = LoopRecipeFile.highest_written(
+            self.outputs, conf, cli_args)
+        if highest is None:
+            'no output'
+        return highest(conf, cli_args)
+
 
 class Translate(Rule):
     pass
