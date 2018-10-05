6d80da186030354f5adb9d484adfb7fc4816643d
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2018-10-05 18:08:17 +0300

    WIP: ChooseModel with DeadEndPipe

--- textpipes/lmclean.py
+++ textpipes/lmclean.py
@@ -1,4 +1,6 @@
+from .core.platform import run
 from .core.recipe import Rule, LoopRecipeFile
+from .components.core import DeadEndPipe, SingleCellComponent
 from .external import simple_external
 
 # args: --emb-dim N --hidden-dim N --n-layers N --dropout-p X --learning-rate X
@@ -11,23 +13,25 @@ class TrainLmclean(Rule):
                  model_seckey,
                  save_every=2000, final=40000, argstr='',
                  **kwargs):
-        loop_indices = [x + save_every for x in range(0, final, save_every)]
+        self.loop_indices = [x + save_every for x in range(0, final, save_every)]
         self.models = LoopRecipeFile.loop_output(
-            model_seckey[0], model_seckey[1], loop_indices)
+            model_seckey[0], model_seckey[1], self.loop_indices)
         self.train_file = train_file
         self.dev_file = dev_file
         self.pipe_file = pipe_file
+        self.save_every = save_every
+        self.final = final
         self.argstr = argstr
 
         inputs = [train_file, dev_file]
-        outputs = self.models
+        outputs = self.models + [pipe_file]
         super().__init__(inputs, outputs, **kwargs)
         self.add_opt_dep('lmclean-train', binary=True)
 
     def make(self, conf, cli_args):
         model_base, _ = self.models[0](conf, cli_args).rsplit('.', 1)
         run('lmclean-train'
-            ' {model_base} {train_file} {dev_file} --save-every {save_every} {argstr}'
+            ' {model_base} {train_file} {dev_file} --save-every {save_every} {argstr}'
             ' >> {pipe_file} 2>&1'.format(
                 model_base=model_base,
                 train_file=self.train_file(conf, cli_args),
@@ -48,10 +52,30 @@ class TrainLmclean(Rule):
         return highest(conf, cli_args)
 
 EvalLmclean = simple_external(
-    'EvalLmclean', ['model' 'testfile'], ['scorefile'],
+    'EvalLmclean', ['model', 'testfile'], ['scorefile'],
     'lmclean-eval {model} {testfile} --print-params {argstr} > {scorefile}',
-    auto_log=False)
+    autolog_stdout=False)
 
 ScoreLmclean = simple_external(
-    'ScoreLmclean', ['model' 'testfile'], ['scorefile'],
+    'ScoreLmclean', ['model', 'testfile'], ['scorefile'],
     'lmclean-score {model} {testfile} --output {scorefile} {argstr}')
+
+class ChooseModel(DeadEndPipe):
+    def __init__(self, model_seckey, eval_seckey, outfile, loop_indices,
+                 **kwargs):
+        self.loop_indices = loop_indices
+        self.models = LoopRecipeFile.loop_output(
+            model_seckey[0], model_seckey[1], self.loop_indices)
+        self.evals = LoopRecipeFile.loop_output(
+            eval_seckey[0], eval_seckey[1], self.loop_indices)
+        self.outfile = outfile
+
+        def ChooseModelComponent(SingleCellComponent):
+            def __call__(self, stream, side_fobjs=None,
+                         config=None, cli_args=None):
+                pass
+
+            def post_make(self, side_fobjs):
+                pass
+
+        super().__init__([ChooseModelComponent()], self.evals, **kwargs)
