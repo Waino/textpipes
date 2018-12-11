7b43cbcb5380b769c2ca0b283bbf9635638b6ecc
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2018-12-11 09:54:10 +0200

    fix to error message

--- textpipes/core/recipe.py
+++ textpipes/core/recipe.py
@@ -310,7 +310,8 @@ class Recipe(object):
                 if any(inp not in known for inp in rule.inputs):
                     # has inputs that are not yet part of the plan
                     remaining.add(cursor)
-                    not_yet[cursor] = [inp not in known for inp in rule.inputs]
+                    not_yet[cursor] = [inp for inp in rule.inputs
+                                       if inp not in known]
                     continue
                 known.update(rule.outputs)
                 not_done = tuple(inp for inp in rule.inputs
@@ -345,9 +346,9 @@ class Recipe(object):
                     concrete=concrete,
                     overrides=overrides))
             if len(remaining) == len(needed):
-                err_str = ['\n'.join('{} depends on: {}'.format(
-                    cursor, not_yet[cursor]))
-                    for cursor in remaining]
+                err_str = '\n'.join(['{} depends on: {}'.format(
+                    cursor, not_yet[cursor])
+                    for cursor in remaining])
                 raise Exception('unmet dependencies:\n{}'.format(err_str))
             needed = remaining
 
