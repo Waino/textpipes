a87ab4dacfc885e015f4c873a4ee9b0ebbbf75e4
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2018-12-11 16:33:23 +0200

    import

--- textpipes/core/__init__.py
+++ textpipes/core/__init__.py
@@ -1,4 +1,4 @@
 from .recipe import Recipe, IndirectRecipeFile, WildcardLoopRecipeFile
 from .configuration import Config
 from .utils import safe_zip, progress
-from . import utils
+from . import utils, platform
