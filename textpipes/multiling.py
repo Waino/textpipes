d860969fbe51ddf5cae0b744ed774598b427b306
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2019-05-15 11:13:02 +0300

    keep pivot when triangulating

--- textpipes/multiling.py
+++ textpipes/multiling.py
@@ -19,16 +19,20 @@ from .core.utils import safe_zip, progress, FIVEDOT
 
 # Rule, not Component (input pairs not synchronous)
 class TriangulateParallel(Rule):
-    def __init__(self, pivot_a, inp_a, pivot_b, inp_b, out_a, out_b):
+    def __init__(self, pivot_a, inp_a, pivot_b, inp_b, out_a, out_b, out_pivot=None):
         self.pivot_a = pivot_a
         self.inp_a = inp_a
         self.pivot_b = pivot_b
         self.inp_b = inp_b
         self.out_a = out_a
         self.out_b = out_b
+        self.out_pivot = out_pivot
+        outputs = [out_a, out_b]
+        if out_pivot is not None:
+            outputs.append(out_pivot)
         super().__init__(
             [pivot_a, inp_a, pivot_b, inp_b],
-            [out_a, out_b])
+            outputs)
 
     def make(self, conf, cli_args=None):
         pivot_a = self.pivot_a.open(conf, cli_args, mode='r')
@@ -38,15 +42,17 @@ class TriangulateParallel(Rule):
 
         out_a = self.out_a.open(conf, cli_args, mode='w')
         out_b = self.out_b.open(conf, cli_args, mode='w')
+        if self.out_pivot is not None:
+            out_pivot = self.out_pivot.open(conf, cli_args, mode='w')
 
         map_a = {}
         for (pivot, a) in safe_zip(pivot_a, inp_a):
-            pivot = ''.join([x for x in pivot.lower() if x in FILTER_ALPHA])
-            map_a[pivot] = a
+            pivot_lc = ''.join([x for x in pivot.lower() if x in FILTER_ALPHA])
+            map_a[pivot_lc] = a
 
         for (pivot, b) in safe_zip(pivot_b, inp_b):
-            pivot = ''.join([x for x in pivot.lower() if x in FILTER_ALPHA])
-            a = map_a.get(pivot, None)
+            pivot_lc = ''.join([x for x in pivot.lower() if x in FILTER_ALPHA])
+            a = map_a.get(pivot_lc, None)
             if a is None:
                 # no match
                 continue
@@ -54,9 +60,14 @@ class TriangulateParallel(Rule):
             out_a.write('\n')
             out_b.write(b)
             out_b.write('\n')
+            if self.out_pivot is not None:
+                out_pivot.write(pivot)
+                out_pivot.write('\n')
 
         for fobj in (pivot_a, inp_a, pivot_b, inp_b, out_a, out_b):
             fobj.close()
+        if self.out_pivot is not None:
+            out_pivot.close()
 
 
 class FastAlign(Rule):
